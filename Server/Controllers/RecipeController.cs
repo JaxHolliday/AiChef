c6be374076f74e553db32ef92c0dc8a2cb5e81fa
JaxHolliday <jaxholliday@gmail.com>
2023-08-02 16:47:20 -0400

    added Recipe Controller

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,19 @@
+﻿using AiChef.Shared;
+using AiChef.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AiChef.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        //post to send the information
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
+        {
+            return SampleData.RecipeIdeas;
+        }
+    }
+}
