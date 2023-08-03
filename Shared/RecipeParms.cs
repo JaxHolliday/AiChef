48d460d625fe1c9b1d7b5702ef81df319909ba24
JaxHolliday <jaxholliday@gmail.com>
2023-08-03 18:02:46 -0400

    Api controler finished

--- Shared/RecipeParms.cs
+++ Shared/RecipeParms.cs
@@ -8,7 +8,7 @@ namespace AiChef.Shared
 {
     public class RecipeParms
     {
-        public string? MealTime { get; set; }
+        public string? MealTime { get; set; } = "Breakfast";
         public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
         public string? SelectedIdea { get; set; }
     }
