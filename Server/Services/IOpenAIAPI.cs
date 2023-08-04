e101d5a8f530f5449004597a3faec3345ab0193b
JaxHolliday <jaxholliday@gmail.com>
2023-08-04 17:59:57 -0400

    added AI Interface

---
+++ Server/Services/IOpenAIAPI.cs
@@ -0,0 +1,11 @@
+﻿using AiChef.Shared;
+
+namespace AiChef.Server.Services
+{
+    public interface IOpenAIAPI
+    {
+        Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients);
+
+
+    }
+}
